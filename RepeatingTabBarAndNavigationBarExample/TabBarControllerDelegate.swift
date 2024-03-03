5fd647838d80d46c3e850823a589ee8870a89070
Dmitry Poryadin <dporyadin11@gmail.com>
2024-03-03 10:42:30 +0300

    initial commit

---
+++ RepeatingTabBarAndNavigationBarExample/TabBarControllerDelegate.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+final class TabBarControllerDelegate: NSObject, UITabBarControllerDelegate {
+    func tabBarController(_ tabBarController: UITabBarController, animationControllerForTransitionFrom fromVC: UIViewController, to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        ViewControllerAnimation()
+    }
+}
--- RepeatingTabBarAndNavigationBarExample/ViewController.swift
+++ RepeatingTabBarAndNavigationBarExample/ViewController.swift
@@ -1,19 +1,42 @@
-//
-//  ViewController.swift
-//  RepeatingTabBarAndNavigationBarExample
-//
-//  Created by Дмитрий Порядин on 02.03.2024.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupNavigationBarApperiance()
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        /*
+         example of simple modal navigation
+        let vc = ViewController()
+        vc.view.backgroundColor = UIColor.random()
+        present(vc, animated: true)
+         */
+    }
+    
+    private func setupNavigationBarApperiance() {
+        let navigationBarApperiance = UINavigationBarAppearance()
+        navigationBarApperiance.configureWithDefaultBackground()
+        navigationBarApperiance.backgroundColor = .systemYellow
+        navigationBarApperiance.titleTextAttributes = [
+            NSAttributedString.Key.foregroundColor: UIColor.systemBlue
+        ]
+        
+        navigationItem.standardAppearance = navigationBarApperiance
+        navigationItem.compactAppearance = navigationBarApperiance
+        navigationItem.scrollEdgeAppearance = navigationBarApperiance
+        
+        navigationItem.leftBarButtonItem?.tintColor = .systemBackground
+        navigationItem.rightBarButtonItem?.tintColor = .systemBackground
+        
+        navigationController?.navigationBar.tintColor = .systemBackground
+        navigationController?.navigationBar.isTranslucent = false
+        
+        title = "Test"
+        navigationItem.prompt = "Navigation prompts appear at the top"
+    }
 }
-
